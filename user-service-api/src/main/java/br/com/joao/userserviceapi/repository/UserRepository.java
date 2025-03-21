e1425f4fae8e97b50ce964deae1d4a7ab2dc0ae5
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2025-03-21 19:06:08 -0300

    feat: add user repository

---
+++ user-service-api/src/main/java/br/com/joao/userserviceapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.joao.userserviceapi.repository;
+
+import br.com.joao.userserviceapi.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}

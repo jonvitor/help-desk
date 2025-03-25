1361c9c789950d7c650bfcacb8d8f401ec72262d
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2025-03-25 14:23:01 -0300

    feat: add find all users

--- user-service-api/src/main/java/br/com/joao/userserviceapi/mapper/UserMapper.java
+++ user-service-api/src/main/java/br/com/joao/userserviceapi/mapper/UserMapper.java
@@ -6,6 +6,8 @@ import models.responses.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
@@ -17,6 +19,8 @@ import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 public interface UserMapper {
 
     UserResponse fromEntity(final User entity);
+
+    List<UserResponse> fromEntities(final List<User> entity);
 
     @Mapping(target = "id", ignore = true)
     User toEntity(final CreateUserRequest request);
--- user-service-api/src/main/java/br/com/joao/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/com/joao/userserviceapi/service/UserService.java
@@ -9,6 +9,8 @@ import models.responses.UserResponse;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -35,5 +37,9 @@ public class UserService {
                 throw new DataIntegrityViolationException("Email " + email + " already exists");
             }
         });
+    }
+
+    public List<UserResponse> findAll() {
+        return userMapper.fromEntities(userRepository.findAll());
     }
 }

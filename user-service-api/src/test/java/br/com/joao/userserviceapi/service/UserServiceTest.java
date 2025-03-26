ea4405f885b0a91741617bd8df2a521207c90c89
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2025-03-26 11:57:14 -0300

    feat: add test for the find all method of the UserService class

--- user-service-api/src/test/java/br/com/joao/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/joao/userserviceapi/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -53,5 +54,19 @@ class UserServiceTest {
         assertThrows(ResourceNotFoundException.class, () -> service.findById("1"));
         verify(repository, times(1)).findById(anyString());
         verify(mapper, times(0)).fromEntity(any(User.class));
+    }
+
+    @Test
+    void whenCallFindAllThenReturnsTheReturnListOfUserResponse() {
+        when(repository.findAll()).thenReturn(List.of(new User(), new User()));
+        when(mapper.fromEntities(anyList())).thenReturn(List.of(mock(UserResponse.class), mock(UserResponse.class)));
+
+        final var response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(2, response.size());
+        assertEquals(UserResponse.class, response.get(0).getClass());
+        verify(repository, times(1)).findAll();
+        verify(mapper, times(1)).fromEntities(anyList());
     }
 }

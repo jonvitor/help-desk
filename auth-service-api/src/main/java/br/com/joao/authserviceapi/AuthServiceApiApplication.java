4aa4117d8719b7060d48fa0b1a06d1969893ec49
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2025-03-27 17:16:11 -0300

    chore: initiate auth-service-api

---
+++ auth-service-api/src/main/java/br/com/joao/authserviceapi/AuthServiceApiApplication.java
@@ -0,0 +1,10 @@
+package br.com.joao.authserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+    public static void main(String[] args) { SpringApplication.run(AuthServiceApiApplication.class, args); }
+}

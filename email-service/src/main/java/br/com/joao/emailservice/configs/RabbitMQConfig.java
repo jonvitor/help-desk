bd1fbb899fd3c9a079a4be85aa34d511648583cd
João Vitor da Anunciação <joao.anunciacao@compasso.com.br>
2025-04-11 12:40:23 -0300

    feat: add rabbitmq config in email service

---
+++ email-service/src/main/java/br/com/joao/emailservice/configs/RabbitMQConfig.java
@@ -0,0 +1,21 @@
+package br.com.joao.emailservice.configs;
+
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public SimpleMessageConverter messageConverter() {
+        SimpleMessageConverter messageConverter = new SimpleMessageConverter();
+        messageConverter.setAllowedListPatterns(
+                List.of("models.*", "java.util.*", "java.time.*", "java.lang.*")
+        );
+
+        return messageConverter;
+    }
+}
